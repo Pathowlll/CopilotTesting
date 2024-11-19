92062c8d70ef970ebfb0172a023f37e3450fe751
Pathowlll <patryk.podlewski@kainos.com>
2024-11-19 11:46:45 +0000

    added skills.js

---
+++ skills.js
@@ -0,0 +1,14 @@
+function calculateNumbers(var1, var2) {
+  return var1 + var2;
+}
+
+function calculateNumbers(var1, var2) {
+    return var1 + var2;
+}
+
+function drawACircle(radius) {
+    return Math.PI * radius * radius;
+}
+
+console.log(drawACircle(5))
+
